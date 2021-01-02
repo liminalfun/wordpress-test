4d71723bc402e8bb3c1ed6c0bc275316134b0231
Alex <xelanawotka@gmail.com>
2021-01-01 22:56:25 -0500

    new initial commit !!! woo hoo !! it works !!!

--- .env.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  BROWSERSYNC_PROXY: "localhost:3000",
-};
---
+++ granola-test/footer.php
@@ -0,0 +1,6 @@
+<?php echo \Granola\render('assets/components/footer'); ?>
+<?php wp_footer(); ?>
+<?php echo \Granola\render('template-parts/serviceworker'); ?>
+
+</body>
+</html>
